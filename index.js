b5232d582fa794b527b2d13812fc98d262a9af66
moeac <moecheaib@hotmail.com>
2022-01-01 00:50:09 +0800

    index.js finished with all inquirer prompts

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function addManager() {
     return inquirer.prompt (
 
         [
+
             {
                 name: "name",
                 type: "input",
@@ -21,7 +22,7 @@ function addManager() {
                     if (managerName) {
                         return true;
                     } else {
-                        console.log("I need a name! quit playin");
+                        console.log("give me a name pls");
                         return false;
                     }
                 }
@@ -51,19 +52,19 @@ function addManager() {
                     if (valid) {
                         return true;
                     } else {
-                        console.log("just put a legit email so i can send you premium content");
+                        console.log("please enter a genuine email before i genuinely hang myself");
                         return false;
                     }
                 }
             },
 
             {
-                name: 'offcieNumber',
+                name: 'officeNumber',
                 type: 'input',
                 message: "And finally please enter the office number",
                 validate: (offNum) => {
                     if (isNaN(offNum)) {
-                        console.log("m8 enter an office number so i can call you after hours");
+                        console.log("pls give me a number to call, it gets lonely sometimes");
                         return false;
                     } else {
                         return true;
@@ -83,6 +84,158 @@ function addManager() {
         employeeList();
     });
 };
+
+function addEngineer() {
+    return inquirer.prompt(
+
+        [
+
+            {
+                name: "name",
+                type: "input",
+                message: "Who are the engineers around here?",
+                validate: engineerName => {
+                    if (engineerName) {
+                        return true;
+                    } else {
+                        console.log("give me a name pls");
+                        return false;
+                    }
+                }
+            },
+
+            {
+                name: "id",
+                type: "input",
+                message: "What is the engineer's ID?",
+                validate: engineerID => {
+                    if (isNaN(engineerID)) {
+                        console.log("I need an ID before ID -stroy your face, im kidding but pls give me an id otherwise we're just gonna loop forever until one of us dies");
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            },
+
+            {
+                name: 'email',
+                type: 'input',
+                message: "Please give me the engineer's email",
+                validate: email => {
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+                    if (valid) {
+                        return true;
+                    } else {
+                        console.log("please enter a genuine email before i genuinely hang myself");
+                        return false;
+                    }
+                }
+            },
+
+            {
+                name: 'gitHub',
+                type: 'input',
+                message: "Please give me the engineer's gitHub username",
+                validate: gitHubName => {
+                    if (gitHubName) {
+                        return true;
+                    } else {
+                        console.log("give me a github name pls");
+                        return false;
+                    }
+                }
+            }
+
+        ]
+    )
+    .then (engineerData => {
+            const { name, id, email, gitHub } = engineerData;
+            const engineer = new Engineer (name, id, email, gitHub);
+            console.log(engineer);
+
+            theTeam.push(engineer);
+            employeeList();
+    });
+};
+
+function addIntern() {
+    return inquirer.prompt(
+
+        [
+
+            {
+                name: "name",
+                type: "input",
+                message: "Who are the interns around here?",
+                validate: internName => {
+                    if (internName) {
+                        return true;
+                    } else {
+                        console.log("give me a name pls");
+                        return false;
+                    }
+                }
+            },
+
+            {
+                name: "id",
+                type: "input",
+                message: "What is the intern's ID?",
+                validate: internID => {
+                    if (isNaN(internID)) {
+                        console.log("I need an ID before ID -stroy your face, im kidding but pls give me an id otherwise we're just gonna loop forever until one of us dies");
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            },
+
+            {
+                name: 'email',
+                type: 'input',
+                message: "Please give me the slave's email",
+                validate: email => {
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+                    if (valid) {
+                        return true;
+                    } else {
+                        console.log("please enter a genuine email before i genuinely hang myself");
+                        return false;
+                    }
+                }
+            },
+
+            {
+                name: 'school',
+                type: 'input',
+                message: "Which school is the intern going to?",
+                validate: schoolName => {
+                    if (schoolName) {
+                        return true;
+                    } else {
+                        console.log("give me a school name pls");
+                        return false;
+                    }
+                }
+            }
+
+        ]
+    )
+    .then (internData => {
+            const { name, id, email, gitHub } = internData;
+            const intern = new Intern (name, id, email, gitHub);
+            console.log(intern);
+
+            theTeam.push(intern);
+            employeeList();
+    });
+
+};
+        
 
 function employeeList() {
     return inquirer.prompt (
@@ -105,7 +258,19 @@ function employeeList() {
                 addIntern();
                 break;
             default:
-                generateTeam();
+                generateTeam(theTeam);
             }
     });
 };
+
+function generateTeam() {
+    fs.writeFile("./dist/TheTeam.html", data, (err) => 
+    err ? console.log(err) : console.log("We have generated a 'team'!"));
+}
+
+function init() {
+    console.log('alright lets make a team');
+    addManager();
+}
+
+init();
