61b956b863084f2294e876898b3eba26fef15aa0
moeac <moecheaib@hotmail.com>
2022-01-04 02:25:22 +0800

    finised the renderHTML page

--- src/renderHTML.js
+++ src/renderHTML.js
@@ -1,11 +1,13 @@
+// empty array to push html to
 const employees = [];
 
+// fills out manager card and pushed to employees array
 function managerCard(manager) {
     let managerHtml = `
     <div class="card restraint m-4 p-4">
         <div class="card-image">
             <figure class="image">
-                <img src="images/projectmanagersq.jpg" alt="Manager">
+                <img src="../images/projectmanagersq.jpg" alt="Manager">
             </figure>
         </div>
         <div class="card-content">
@@ -30,12 +32,13 @@ function managerCard(manager) {
     employees.push(managerHtml);
 }
 
+// fills out engineer card and pushed to employees array
 function engineerCard(engineer) {
     let engineerHtml = `
     <div class="card restraint m-4 p-4">
         <div class="card-image">
             <figure class="image">
-                <img src="images/developer3.jpg" alt="Developer">
+                <img src="../images/developer3.jpg" alt="Developer">
             </figure>
         </div>
         <div class="card-content">
@@ -52,7 +55,7 @@ function engineerCard(engineer) {
             </div>
             <div class="content has-text-left">
                 <a href="mailto:${engineer.email}"><span class="icon"><i class="fas fa-envelope"></i></span>${engineer.email}</a><br>
-                <a href="https://github.com/${engineer.gitHub}"><span class="icon"><i class="fab fa-github"></i></span>${engineer.gitHub}</a>
+                <a href="https://github.com/${engineer.gitHub}" target="_blank"><span class="icon"><i class="fab fa-github"></i></span>${engineer.gitHub}</a>
             </div>
         </div>
     </div>
@@ -60,12 +63,13 @@ function engineerCard(engineer) {
     employees.push(engineerHtml);
 }
 
+// fills out slave card and pushed to employees array
 function internCard(intern) {
     let internHtml = `
     <div class="card restraint m-4 p-4">
         <div class="card-image">
             <figure class="image">
-                <img src="images/slave.png" alt="Intern">
+                <img src="../images/slave.png" alt="Intern">
             </figure>
         </div>
         <div class="card-content">
@@ -82,7 +86,7 @@ function internCard(intern) {
             </div>
             <div class="content has-text-left">
                 <a href="mailto:${intern.email}"><span class="icon"><i class="fas fa-envelope"></i></span>${intern.email}</a><br>
-                <a href="https://github.com/${intern.school}"><span class="icon"><i class="fas fa-graduation-cap"></i></span>${intern.school}</a>
+                <a href="https://www.w3schools.com/"><span class="icon"><i class="fas fa-graduation-cap"></i></span>${intern.school}</a>
             </div>
         </div>
     </div>
@@ -90,7 +94,8 @@ function internCard(intern) {
     employees.push(internHtml);
 }
 
-function employeePage(team) {
+// provides the head of the html page with all stylesheets mainly using bulma and runs generateCards function to add the employee cards to the body
+function renderHTML(team) {
     return  `
     <!DOCTYPE html>
     <html lang="en">
@@ -99,10 +104,10 @@ function employeePage(team) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Big Team Tings</title>
-        <link rel="shortcut icon" href="./images/wholesomebabyorangutan.jpg" type="image/x-icon">
+        <link rel="shortcut icon" href="../images/wholesomebabyorangutan.jpg" type="image/x-icon">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"/>
-        <link rel="stylesheet" href="./assets/style.css"/>
+        <link rel="stylesheet" href="../assets/style.css"/>
         <script src="https://kit.fontawesome.com/8c6fbba865.js" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600&display=swap" rel="stylesheet">
     </head>
@@ -114,7 +119,7 @@ function employeePage(team) {
 
         <div class="container">
 
-            ${team}
+            ${generateCards(team)}
 
         </div>
     </body>
@@ -122,30 +127,28 @@ function employeePage(team) {
     `
 }
 
-function generateCards(employees) {
+// for every team member matches their role and fills in the cards
+function generateCards(team) {
 
-    const role = employees.getRole();
+    for (let i = 0; i < team.length; i++) {
 
-    if (role === "Manager") {
-        managerCard(manager)
+        if (team[i].getRole() === "Manager") {
+            managerCard(team[i])
+        }
+
+        if (team[i].getRole() === "Engineer") {
+            engineerCard(team[i])
+        }
+
+        if (team[i].getRole() === "Intern") {
+            internCard(team[i])
+        }
+    
     }
-
-    if (role === "Engineer") {
-        engineerCard(engineer)
-    }
-
-    if (role === "Intern") {
-        internCard(intern)
-    }
-
-    employeePage(employees);
+    // joins the items in the array together
+    return employees.join();
 }
 
 
-// function x
-// if role = employeeX
-// run generateemployeeX and push to array
-// 
-// run function to generate everything
 
 module.exports = renderHTML;
